f3a117d3a3b9367536b6ca10c663af047158d79a
psyoon12 <psyoon12a@gmail.com>
2022-05-07 14:32:11 -0400

    Volume

---
+++ Assets/Scripts/PlayerAttackSound.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackSound : MonoBehaviour
+{
+    [SerializeField] AudioClip shootSound;
+    private AudioSource src;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void PlaySound()
+    {
+        src.PlayOneShot(shootSound);
+    }
+}
